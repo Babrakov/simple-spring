d315f5821659687c63e6b92c180d3f101833f571
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-01-06 18:57:23 +0300

    Initial commit

---
+++ src/main/java/com/b3rz3rk/spring/Application.java
@@ -0,0 +1,13 @@
+package com.b3rz3rk.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/b3rz3rk/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b3rz3rk.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
